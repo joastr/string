c821bdce614ea013286d01ea3f26162bc2f7e5a2
joanna Strzelecka <joastr1@gmail.com>
2016-11-28 13:13:29 +0100

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// plik scripts.js
+
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'zielone słonie';
+var animal = animal.toUpperCase();
+console.log(animal.toUpperCase());
+
+//lub console.log(elephants);
+
+var text1 = text.replace('Papugi', animal);
+console.log(text1); 
+		 
+console.log(text1.substr(0, text1.length/2));	 
+
+
+
